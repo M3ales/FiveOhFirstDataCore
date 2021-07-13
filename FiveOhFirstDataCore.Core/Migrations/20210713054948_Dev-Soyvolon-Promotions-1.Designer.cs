9d0a1c73369876293d1ee6b810e46303999adf8c
Andrew Bounds <andybounds@gmail.com>
2021-07-13 02:08:53 -0400

    Lots of work into the promotion boards has been done.

--- FiveOhFirstDataCore.Core/Migrations/20210713054948_Dev-Soyvolon-Promotions-1.Designer.cs
+++ FiveOhFirstDataCore.Core/Migrations/20210713054948_Dev-Soyvolon-Promotions-1.Designer.cs
@@ -5,14 +5,16 @@ using FiveOhFirstDataCore.Core.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FiveOhFirstDataCore.Core.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210713054948_Dev-Soyvolon-Promotions-1")]
+    partial class DevSoyvolonPromotions1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
